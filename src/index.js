e3a85f191f1de147a1b2aaebf10c907b5e811c8e
Musirika <673793@cognizant.com>
2021-05-26 01:06:38 +0530

    removed logger for production

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import rootReducer from './reducers/index';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development'){
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
